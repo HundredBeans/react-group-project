7900525cc8b8a176d0c7f106d2bb106474775db5
Gading09 <hedy@alterra.id>
2020-01-10 13:35:59 +0700

    update

--- src/routes/main-route.jsx
+++ src/routes/main-route.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from '../pages/home';
-import SignIn from '../pages/signin';
 import Profile from '../pages/profile';
-import LogOut from '../pages/logout';
 import NotMatch from '../pages/nomatch';
 import ComingSoon from '../pages/comingsoon';
 
@@ -13,7 +11,6 @@ const MainRoute = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/profile" component={Profile} />
-                <Route exact path="/logout" component={LogOut} />
                 <Route exact path="/blank" component={ComingSoon} />
                 <Route component ={NotMatch}/>
             </Switch>
